5a3116625417c9748d2450f5497546ba07132a0a
brandonwherry <bwh1126@gmail.com>
2023-10-25 15:36:37 -0500

    added nvchad config

--- nvim/lua/custom/plugins.lua
+++ nvim/lua/custom/plugins.lua
@@ -1,8 +1,18 @@
-local cmp = require "cmp"
-
 local plugins = {
-  "christoomey/vim-tmux-navigator",
-  lazy = false,
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+          "pyright",
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
 }
-
 return plugins
